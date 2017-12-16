39e757a147dda2fddfca3908738e902c54fc449e
austintedwards <austintedwards@gmail.com>
2017-12-16 11:42:51 -0700

    working with active link

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,12 +5,20 @@ import Title from './Title';
 
 class App extends Component {
    myFunction() {
-    var x = document.getElementById("myTopnav");
+    let x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
         x.className = "topnav";
     }
+}
+
+active(n){
+  let x = document.getElementsByTagName("a")
+  console.log(x,n)
+  x.forEach((a)=>{
+    console.log(a)
+  })
 }
   render() {
     return (
@@ -18,9 +26,10 @@ class App extends Component {
         <header className="App-header">
           <div className="topnav" id="myTopnav">
             <a href="#home" className="active">Home</a>
-            <a href="#news">News</a>
-            <a href="#contact">Contact</a>
+            <a href="#portfolio" onClick={()=>this.active(1)}>Portfolio</a>
+            <a href="#resume">Resume</a>
             <a href="#about">About</a>
+            <a href="#drawings">Drawings</a>
             <a href="javascript:void(0);" className="icon" onClick={()=>this.myFunction()}>&#9776;</a>
           </div>
                 </header>
